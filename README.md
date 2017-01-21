3a661e05cb2773da3ef33182067c5c6a1fbfab4a
Will Hurley <wjhurley@comcast.net>
2017-01-21 00:27:57 -0800

    updates index.js to first functioning version and README file with setup instructions

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # WordPress-Post-Automater
-Selenium-webdriver application for automating WordPress post edits.
+Selenium WebDriver application for automating WordPress post edits.
+## Getting Started
+**This will only set up Selenium WebDriver for Chrome on Windows. Please see the official [SeleniumHQ website](http://www.seleniumhq.org/) for help with other browsers and operating systems.**
+
+Prerequisite: [Node.js](https://nodejs.org/en/) must be installed.
+
+1. Create a project folder on your computer.
+2. Clone this repository or fork and clone your forked repository to the project folder.
+3. Navigate to the new wordpress-post-automater folder and open a Command Line.
+4. From the terminal run: `npm install` to install dependencies.
+5. Once installation is complete, go to [https://sites.google.com/a/chromium.org/chromedriver/downloads](https://sites.google.com/a/chromium.org/chromedriver/downloads) to download the latest version of ChromeDriver.
+6. Once downloaded, open the zip file and extract to the wordpress-post-automater folder.
+7. Running the application requires 4 arguments:
+    `wordpressURL` - The admin login page for your WordPress site (../wp-login.php)
+    `userName` - The admin user name for your WordPress site
+    `userPassword` - The admin password for your WordPress site
+    `searchQuery` - The string you want to search for and replace in your WordPress posts
+8. The terminal command will look like this: `node index.js wordpressURL userName userPassword searchQuery`
+
+Additional Selenium WebDriver documentation can be found [here](http://seleniumhq.github.io/selenium/docs/api/javascript/index.html).
--- index.js
+++ index.js
@@ -9,27 +9,53 @@ var driver = new webdriver.Builder()
 var input = require('./node_modules/selenium-webdriver/lib/input'),
     Key = input.Key;
 
+var wordpressURL = process.argv[2];
+var userName = process.argv[3];
+var userPassword = process.argv[4];
+var defaultTimeOut = 20000;
 var fixedText = '';
+var numberChallenges = 0;
 
-driver.get('http://www.testurl.com');
-driver.wait(until.titleContains('The Library'), 1000);
-driver.findElement(By.id('user_login')).sendKeys('userName');
-driver.findElement(By.id('user_pass')).sendKeys('password');
+//set timeout for waiting on DOM element
+driver.manage().timeouts().implicitlyWait(defaultTimeOut);
+driver.manage().timeouts().pageLoadTimeout(defaultTimeOut);
+//navigate to Wordpress login page
+driver.get(wordpressURL);
+//driver.wait(until.titleContains('The Library'), defaultTimeOut);
+driver.findElement(By.id('user_login')).sendKeys(userName);
+driver.findElement(By.id('user_pass')).sendKeys(userPassword);
 driver.findElement(By.id('wp-submit')).click();
-driver.findElement(By.partialLinkText('Posts')).click();
+//search for the desired posts
+driver.findElement(By.xpath('//li[@id="menu-posts"]/a/div[3]')).click();
+driver.findElement(By.linkText('All Posts')).click();
 driver.findElement(By.id('post-search-input')).sendKeys('©');
 driver.findElement(By.id('search-submit')).click();
-driver.findElement(By.className('row-title')).click();
-//editorContents = editorContents.replace('©', '&copy;');
-driver.findElement(By.className('wp-editor-area')).getText().then( function(text) {
-  let copyRegExp = /[©]/g;
-  if(text) {
-    fixedText = text.replace(copyRegExp, '&copy;');
-    return driver.findElement(By.className('wp-editor-area')).sendKeys(Key.chord(Key.CONTROL, 'a'), fixedText);
-  } else {
-    console.log('fail -- ' + text);
+//scrape the number of search results found
+driver.findElement(By.className('displaying-num')).getText().then( function(text) {
+  //use search results to determine how many posts to edit
+  numberChallenges = Number(text.substring(0, text.indexOf(" ")));
+  //loop to run on all posts that meet search criteria
+  for(let i = 0; i < numberChallenges; i++) {
+    driver.findElement(By.className('row-title')).click();
+    driver.findElement(By.className('wp-editor-area')).getText().then( function(text) {
+      let copyRegExp = /[©]/g;
+      let fixedTextArray = [];
+      if(text) {
+        fixedText = text.replace(copyRegExp, '&copy;');
+        driver.findElement(By.id('content')).clear();
+        for(let j = 0; j < fixedText.length; j += 64) {
+          driver.findElement(By.id('content')).sendKeys(fixedText.substr(j, Math.min(64, fixedText.length - j)));
+        };
+      } else {
+        console.log('fail -- ' + text);
+      }
+    });
+    driver.wait(until.elementIsEnabled(driver.findElement(By.id('publish'))), defaultTimeOut);
+    driver.executeScript('window.scrollTo(0, 0);');
+    driver.findElement(By.id('publish')).click();
+    //intentionally re-run search because it's needed in the for loop
+    driver.findElement(By.linkText('All Posts')).click();
+    driver.findElement(By.id('post-search-input')).sendKeys('©');
+    driver.findElement(By.id('search-submit')).click();
   }
-});
-driver.wait(until.elementIsEnabled(driver.findElement(By.id('publish'))), 15000).then( function(isEnabled) {
-  driver.findElement(By.id('publish')).click();
 });
